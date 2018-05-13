f18c37f6fb3de0ccc5154914de55f35a7c93f044
Pedro Ramos <ramuspedro@gmail.com>
2018-05-12 21:16:22 -0300

    refactoring portifolio

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// https://mattboldt.com/demos/typed-js/
+var typed2 = new Typed('.wrap', {
+  strings: ["I'm UI/UX developer.", "I love design apps.", "I develop web/mobile."],
+  typeSpeed: 50,
+  backSpeed: 0,
+  backDelay: 500,
+  startDelay: 1000,
+  loop: true
+});
